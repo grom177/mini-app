89696f10853d8ec189c89473c9cdcd6881e2cedb
grom177 <grom177g@mail.ru>
2025-02-12 01:03:44 +0500

    Fixed manifestUrl in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { TonConnectButton, TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const TON_WALLET = "UQAp3unngYVk9-T7nXjuqdE3Gk1WDXIojiCGzTAvDCwk_3FI"; // Замени на свой адрес
+const TON_WALLET = "UQAp3unngYVk9-T7nXjuqdE3Gk1WDXIojiCGzTAvDCwk_3FI"; // Твой TON-кошелек
 
 const App = () => {
     useEffect(() => {
@@ -15,7 +15,7 @@ const App = () => {
     };
 
     return (
-        <TonConnectUIProvider manifestUrl="https://yourdomain.com/tonconnect-manifest.json">
+        <TonConnectUIProvider manifestUrl="https://mini-app-umber.vercel.app/tonconnect-manifest.json">
             <div className="App">
                 <h1>💰 Пожертвуй 0.25 TON</h1>
                 <TonConnectButton />
